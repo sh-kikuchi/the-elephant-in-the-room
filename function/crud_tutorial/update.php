9c8acb4a27795893c1d35ec1e482944f21496945
sh-kikuchi <shu94hist726@gmail.com>
2023-03-27 23:51:50 +0900

    fix:comment_class_20230327

--- function/crud_tutorial/update.php
+++ function/crud_tutorial/update.php
@@ -1,6 +1,4 @@
 <?php
-//require_onceでDB接続ファイルの呼び出し
-require_once("../../database/db_connect.php");
 require_once("../../class/crud_tutorial/Comment.php");
 $comment = new Comment();
 $comment->update($_POST);
