315303d1f86561d0bb7c9f4ca8a20b0814b99d27
sh-kikuchi <shu94hist726@gmail.com>
2024-04-29 00:24:34 +0900

    renewal sample 20240429

--- axis/database/DataBaseConnect.php
+++ axis/database/DataBaseConnect.php
@@ -11,14 +11,14 @@ class DataBaseConnect {
 
     private function connect() {
 
-    //    $host = $_ENV['DB_HOST'];
-    //    $db   = $_ENV['DB_NAME'];
-    //    $user = $_ENV['DB_USER'];
-    //    $pass = $_ENV['DB_PASS'];
-       $host = 'localhost';
-       $db   = 'test';
-       $user = 'root';
-       $pass = '';
+       $host = $_ENV['DB_HOST'];
+       $db   = $_ENV['DB_NAME'];
+       $user = $_ENV['DB_USER'];
+       $pass = $_ENV['DB_PASS'];
+    //    $host = 'localhost';
+    //    $db   = 'test';
+    //    $user = 'root';
+    //    $pass = '';
        $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
         try {
